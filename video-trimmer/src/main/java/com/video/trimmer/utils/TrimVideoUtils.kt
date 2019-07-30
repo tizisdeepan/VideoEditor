0b0f8151e27190159897f21c88f3569362790ff4
Deepan <deepan.e@zohocorp.com>
2019-07-30 15:28:16 +0530

    Optimisations.

--- video-trimmer/src/main/java/com/video/trimmer/utils/TrimVideoUtils.kt
+++ video-trimmer/src/main/java/com/video/trimmer/utils/TrimVideoUtils.kt
@@ -126,11 +126,9 @@ object TrimVideoUtils {
 
     fun stringForTime(timeMs: Int): String {
         val totalSeconds = timeMs / 1000
-
         val seconds = totalSeconds % 60
         val minutes = totalSeconds / 60 % 60
         val hours = totalSeconds / 3600
-
         val mFormatter = Formatter()
         return if (hours > 0) {
             mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString()
